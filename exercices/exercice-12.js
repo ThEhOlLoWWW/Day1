b7e01bdc3f11b490f6b59619d43aa7a152f583ee
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-08 16:22:00 +0100

    ex12 done

--- exercices/exercice-12.js
+++ exercices/exercice-12.js
@@ -17,4 +17,12 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let annee = 2024;
+
+if (( annee % 4 == 0 && annee % 100 !== 0) || (annee % 400 == 0)) {
+    console.log(annee + " est une annee bissextile")
+} else {
+    console.log(annee + " est nest pas une annee bissextile")
+}
+
+
