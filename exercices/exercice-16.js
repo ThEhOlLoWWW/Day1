359e8f70b00a776f588c33f0571f273f0b886309
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-08 18:09:25 +0100

    ex16 done

--- exercices/exercice-16.js
+++ exercices/exercice-16.js
@@ -14,4 +14,9 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let num = "0612345678"
+if (typeof num === 'string' && num.length === 10){
+    console.log("num valide");
+} else {
+     console.log("num invalide");
+}
